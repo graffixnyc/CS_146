c5a7c4b147060f1869fd07227819564335b07de7
Patrick Hill <graffixnyc@gmail.com>
2019-11-14 10:25:54 -0500

    Added JS examples

--- JS3/script.js
+++ JS3/script.js
@@ -10,6 +10,21 @@ var getURL = () => {
 	alert(document.URL);
 };
 
+function changeDiv() {
+	var myDiv = document.getElementById('myDiv');
+	myDiv.classList.remove('salmon');
+	myDiv.classList.add('purple');
+}
+
+function toggleDiv() {
+	var myDiv = document.getElementById('myDiv');
+	myDiv.classList.toggle('hide');
+}
+
+function clearDiv() {
+	var myDiv = document.getElementById('myDiv');
+	myDiv.innerHTML = '';
+}
 var button2 = document.getElementById('myButton2');
 button2.addEventListener('click', (event) => {
 	alert('I was clicked');
